b3145d2ac1b89030ae32a7d2abfde8dd54556330
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-10-10 00:26:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Your code here
 function saturdayFun(activity = "roller-skate"){
-  return 'This Saturday, I want to ${activity}!'
+  return `This Saturday, I want to ${first}!`
 }
 
 function saturdayFun(first = "roller-skate"){
