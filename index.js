867ea82ecd1fc58955e975c491ac9cf8152d038a
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-10-10 00:26:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,4 @@ function saturdayFun(activity = 'roller-skate') {
   return 'This Saturday, I want to ${activity}!'
 }
 
-function saturdayFun(first = "roller-skate"){
-  return `This Saturday, I want to ${first}!`
-}
+
